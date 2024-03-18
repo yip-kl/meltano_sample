2ed2e0c872d47c4b60588fb4c73b2bc39c36f100
yip <yipkwunleong@gmail.com>
2024-03-18 18:52:00 +0800

    refactor variables

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 source .env
 export MSYS_NO_PATHCONV=1
-docker run --rm --env-file ./.env -v $(pwd)/key-files:/project/key-files meltano_bbm run tap-ga4 target-bigquery
+docker run --rm --env-file ./.env -v $(pwd)/$KEY_FILE_FOLDER:$PROJECT_ROOT/$KEY_FILE_FOLDER $MELTANO_IMAGE run tap-ga4 target-bigquery
